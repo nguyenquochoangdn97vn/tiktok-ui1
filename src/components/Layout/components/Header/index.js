66a1f1824d671bf1a5b038b0e5e21484d323e071
nguyenquochoangdn97vn <bangquangnuhoang2@gmail.com>
2023-04-14 21:15:16 +0700

    xây dựng tiktok1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,35 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* {logo} */}
-                {/* {search} */}
+                <img src={images.logo} alt="tiktok" />
+                <div className={cx('search')}>
+                    <input
+                        placeholder="search acounts videos"
+                        spellCheck={false}
+                    />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon
+                        className={cx('loading')}
+                        icon={faSpinner}
+                    />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
             </div>
         </header>
     );
